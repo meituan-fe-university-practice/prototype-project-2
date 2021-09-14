d4f61828d2e573aedac8f2272f51a8c5b66da964
Breeze-P <zhiyuzhao8@gmail.com>
2021-09-14 19:54:10 +0800

    version 2.1

--- src/features/view_container/index.js
+++ src/features/view_container/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import 'antd/dist/antd.css';
 import './style.css';
 import {Layout} from 'antd';
@@ -34,41 +35,49 @@ const ViewContainer = () => {
     }
 
     return(
-        <Layout style={{ minHeight: '100vh' }}>
-            <Layout className="site-layout">
-                <Header className="site-layout-background" style={{ padding: 0 }}>
-                    <button className="header-left" onClick={sortTimeByTime}>
-                        {(!timeFlag) ?
-                            <span>
-                                <ClockCircleOutlined />
-                                时间排序
-                            </span>
-                            :
-                            <span>
-                                <RedoOutlined />
-                                序号排序
-                            </span>
-                        }
-                    </button>
-                    <div className="header-title">
-                        项目列表
-                    </div>
-                    <div className="header-right">
-                        <span>
-                            <PlusCircleOutlined />
-                            添加项目
-                        </span>
-                    </div>
-                </Header>
-                <Content className="site-layout-background content-container">
-                    <ProjectList projects={projects} />
-                </Content>
-                <Footer style={{ textAlign: 'center' }}>Author: Breeze-P Github:
-                    <a href="https://github.com/Breeze-P" style={{ whiteSpace: "pre" }}>  https://github.com/Breeze-P</a>
-                </Footer>
-            </Layout>
-        </Layout>
-    )
+        <Switch>
+            <Route exact path="/project">
+                <Layout style={{ minHeight: '100vh' }}>
+                    <Layout className="site-layout">
+                        <Header className="site-layout-background" style={{ padding: 0 }}>
+                            <button className="header-left" onClick={sortTimeByTime}>
+                                {(!timeFlag) ?
+                                    <span>
+                                        <ClockCircleOutlined />
+                                        时间排序
+                                    </span>
+                                    :
+                                    <span>
+                                        <RedoOutlined />
+                                        序号排序
+                                    </span>
+                                }
+                            </button>
+                            <div className="header-title">
+                                项目列表
+                            </div>
+                            <div className="header-right">
+                                <span>
+                                    <PlusCircleOutlined />
+                                    添加项目
+                                </span>
+                            </div>
+                        </Header>
+                        <Content className="site-layout-background content-container">
+                            <ProjectList projects={projects} />
+                        </Content>
+                        <Footer style={{ textAlign: 'center' }}>Author: Breeze-P Github:
+                            <a href="https://github.com/Breeze-P" style={{ whiteSpace: "pre" }}>  https://github.com/Breeze-P</a>
+                        </Footer>
+                    </Layout>
+                </Layout>
+            </Route>
+            <Route path="/project/:id">
+                this is a project
+            </Route>
+            <Redirect from="/*" to="/project"/>
+        </Switch>
+    );
 }
 
 export default ViewContainer;
